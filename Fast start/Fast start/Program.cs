82ad40acd56bcafbcd8f4a71fb102c9bff76cb09
Cyril Safin <kirillsafin@nlstar.com>
2017-05-23 20:44:55 +0400

    Работа с циклом ИФ, создана выбиралка фильма.

--- Fast start/Fast start/Program.cs
+++ Fast start/Fast start/Program.cs
@@ -10,7 +10,36 @@ namespace Fast_start
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello world!");
+            Console.WriteLine("Привет! Эта программа поможет тебе подобрать фильм на вечер.");
+            Console.WriteLine("Как тебя зовут?");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Здравствуй, " + name+"!");
+
+            Console.WriteLine("Какой жанр ты бы хотел посмотреть: комедию или драму?");
+            string g = Console.ReadLine();
+            Console.WriteLine("В таком случае, рекомендую посмотреть:");
+            if (g == "комедия" || g == "Комедия")
+            {
+                Console.WriteLine("* Иван васильевич меняет профессию");
+                Console.WriteLine("* Назад в будущее");
+                Console.WriteLine("* В джазе только девушки");
+            }
+            else if (g=="драма" || g=="Драма")
+            {
+                Console.WriteLine("* Зеленая миля");
+                Console.WriteLine("* Форест Гамп");
+                Console.WriteLine("* Побег из Шоушенка");
+            }
+            else if (g != "комедия" || g != "Комедия" || g != "драма" || g != "Драма")
+            {
+                Console.WriteLine("Я не знаю такого жанра(");
+                
+            }
+
+
+            
+            
             Console.ReadKey();
         }
     }
